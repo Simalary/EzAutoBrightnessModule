9880c35a129f8acecaa7350da50bd157a49e7402
Simalary <harper.chris033@live.com>
2018-03-13 22:36:51 -0400

    working-ish

--- EzAutoBrightness.m
+++ EzAutoBrightness.m
@@ -14,10 +14,7 @@
 }
 
 - (BOOL)isSelected {
-	NSString *path = @"/var/mobile/Library/Preferences/com.apple.backboardd.plist";
-	NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
-	BOOL valueForSilent = [[dict valueForKey:@"BKEnableALS"] boolValue];
-	if (valueForSilent) {
+	if ([objc_getClass("PSBrightnessSettingsDetail") autoBrightnessEnabled]) {
 		self.autoBrightness = TRUE;
 	} else {
 		self.autoBrightness = FALSE;
@@ -26,41 +23,12 @@
 }
 
 - (void)setSelected:(BOOL)selected {
-	NSString *path = @"/var/mobile/Library/Preferences/com.apple.backboardd.plist";
-	NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
-	BOOL valueForSilent = [[dict valueForKey:@"BKEnableALS"] boolValue];
-	if (valueForSilent) {
-		NSString *sbPath = @"/var/mobile/Library/Preferences/com.apple.backboardd.plist";
-		NSMutableDictionary *sbDict = [[NSMutableDictionary alloc] initWithContentsOfFile:sbPath];
-		[sbDict setValue:[NSNumber numberWithBool:NO] forKey:@"BKEnableALS"];
-		[sbDict writeToFile:sbPath atomically: YES];
-		notify_post("com.apple.BackBoard/Prefs");
-		notify_post("com.apple.backboardd/Prefs");
-		notify_post("com.apple.backboardd");
-		notify_post("com.apple.BackBoard");
-		notify_post("com.apple.backboardd.BKEnableALS.changed");
-		notify_post("com.apple.BackBoard.BKEnableALS.changed");
-		CFPreferencesAppSynchronize(CFSTR("com.apple.backboardd"));
-		CFPreferencesAppSynchronize(CFSTR("com.apple.BackBoard"));
-		CFRelease(CFSTR("com.apple.backboardd"));
-		CFRelease(CFSTR("com.apple.BackBoard"));
+	if ([objc_getClass("PSBrightnessSettingsDetail") autoBrightnessEnabled]) {
 		selected = FALSE;
+		[objc_getClass("PSBrightnessSettingsDetail") setAutoBrightnessEnabled:FALSE];
 	} else {
-		NSString *sbPath = @"/var/mobile/Library/Preferences/com.apple.backboardd.plist";
-		NSMutableDictionary *sbDict = [[NSMutableDictionary alloc] initWithContentsOfFile:sbPath];
-		[sbDict setValue:[NSNumber numberWithBool:YES] forKey:@"BKEnableALS"];
-		[sbDict writeToFile:sbPath atomically: YES];
-		notify_post("com.apple.BackBoard/Prefs");
-		notify_post("com.apple.backboardd/Prefs");
-		notify_post("com.apple.backboardd");
-		notify_post("com.apple.BackBoard");
-		notify_post("com.apple.backboardd.BKEnableALS.changed");
-		notify_post("com.apple.BackBoard.BKEnableALS.changed");
-		CFPreferencesAppSynchronize(CFSTR("com.apple.backboardd"));
-		CFPreferencesAppSynchronize(CFSTR("com.apple.BackBoard"));
-		CFRelease(CFSTR("com.apple.backboardd"));
-		CFRelease(CFSTR("com.apple.BackBoard"));
 		selected = TRUE;
+		[objc_getClass("PSBrightnessSettingsDetail") setAutoBrightnessEnabled:TRUE];
 	}
 	self.autoBrightness = selected;
 	[super refreshState];
